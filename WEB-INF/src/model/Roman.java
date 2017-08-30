86f27df743c3964f48fe4fc7084843f72834996e
AMANO Masaru <pg2017.educator@gamil.com>
2017-08-31 00:45:04 +0900

    不要なロジックを減らした。

--- WEB-INF/src/model/Roman.java
+++ WEB-INF/src/model/Roman.java
@@ -25,22 +25,17 @@ public class Roman {
 	 */
 	private static String toRoman(int inputNumber){
 
-		if (inputNumber < 6){
-			switch (inputNumber){
-				case 1 : return "Ⅰ";
-				case 2 : return "Ⅱ";
-				case 3 : return "Ⅲ";
-				case 4 : return "Ⅳ";
-				case 5 : return "Ⅴ";
-			}
-		}else{
-			switch (inputNumber){
-				case 6 : return "Ⅵ";
-				case 7 : return "Ⅶ";
-				case 8 : return "Ⅷ";
-				case 9 : return "Ⅸ";
-				case 10 : return "Ⅹ";
-			}
+		switch (inputNumber){
+			case 1 : return "Ⅰ";
+			case 2 : return "Ⅱ";
+			case 3 : return "Ⅲ";
+			case 4 : return "Ⅳ";
+			case 5 : return "Ⅴ";
+			case 6 : return "Ⅵ";
+			case 7 : return "Ⅶ";
+			case 8 : return "Ⅷ";
+			case 9 : return "Ⅸ";
+			case 10 : return "Ⅹ";
 		}
 		return "";
 	}
